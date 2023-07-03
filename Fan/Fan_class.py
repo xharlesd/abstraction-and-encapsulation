581485235d2f2efc37a4682546b057d30989cb9b
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-03 23:51:25 +0800

    modify fan_class.py

--- Fan/Fan_class.py
+++ Fan/Fan_class.py
@@ -1,4 +1,9 @@
 class Fan:
+
+    Slow_FS = 1
+    Medium_FS = 2
+    Fast_FS = 3
+
     # create constructor
     def __init__(self, speed = "Slow", radius = 5, color = "Blue", status = False):
         # instance variables
@@ -24,27 +29,18 @@ class Fan:
     def get_color(self):
         return self.__color
 
-
     # set fan speed
-    def set_speed(self):
-        self.__speed = int(input("Enter fan speed: "))
-        if self.__speed == 1: 
-            self.__speed == "Slow"
-        elif self.__speed == 2: 
-            self.__speed == "Medium"
-        elif self.__speed == 3: 
-            self.__speed == "Fast"
-        else:
-            self.__speed == "Invalid Input"
+    def set_speed(self, speed):
+        self.__speed = speed
 
     # set fan status
-    def set_status(self):
-        self.__status == False
+    def set_status(self, status):
+        self.__status = status
 
     # set fan radius
-    def set_radius(self):
-        self.__radius = float(input("Enter fan radius"))
+    def set_radius(self, radius):
+        self.__radius = radius
 
     # set fan color
-    def set_color(self):
-        self.__color = input('Enter fan color: ')
+    def set_color(self, color):
+        self.__color = color
