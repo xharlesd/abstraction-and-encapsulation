79a454ff4885310e775278df26cae4275a49a608
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-03 17:46:22 +0800

    fan instance variables

--- Fan/Fan_class.py
+++ Fan/Fan_class.py
@@ -2,15 +2,25 @@ class Fan:
     # create constructor
     def __init__(self, speed = "SLOW", radius = 5, color = "Blue", status = False):
         # instance variables
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__status = status
+    
+    # get fan speed
 
-    # create getters
-        # get fan speed
-        # get fan status
-        # get fan radius
-        # get fan color
-    # create setters
-        # set fan speed
-        # set fan status
-        # set fan radius
-        # set fan color
+    # get fan status
+        
+    # get fan radius
+
+    # get fan color
+
+    
+    # set fan speed
+
+    # set fan status
+
+    # set fan radius
+    
+    # set fan color
         ""
