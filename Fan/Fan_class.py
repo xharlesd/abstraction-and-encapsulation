a67fde8e25a5e12dcaed547f32e5f05050cfc6b3
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-03 18:26:38 +0800

    set fan color

--- Fan/Fan_class.py
+++ Fan/Fan_class.py
@@ -9,6 +9,7 @@ class Fan:
     
     # get fan speed
     def get_speed(self):
+        self.__speed
         return self.__speed
 
     # get fan status
@@ -26,12 +27,15 @@ class Fan:
 
     # set fan speed
     def set_speed(self):
+        self.__speed = int(input("Enter fan speed: "))
         if self.__speed == 1: 
             self.__speed == "Slow"
         elif self.__speed == 2: 
             self.__speed == "Medium"
         elif self.__speed == 3: 
             self.__speed == "Fast"
+        else:
+            self.__speed == "Invalid Input"
 
     # set fan status
     def set_status(self):
@@ -39,7 +43,7 @@ class Fan:
 
     # set fan radius
     def set_radius(self):
-        return self.__radius
+        self.__radius = float(input("Enter fan radius"))
 
     # set fan color
     def set_color(self):
