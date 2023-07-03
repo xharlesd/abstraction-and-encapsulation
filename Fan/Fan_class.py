28997784bda0748e2e6c8f0be3bf9d62fc44a735
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-03 17:22:07 +0800

    fan class pseudocode

---
+++ Fan/Fan_class.py
@@ -0,0 +1,13 @@
+class Fan:
+    # create constructor
+    # create getters
+        # get fan speed
+        # get fan status
+        # get fan radius
+        # get fan color
+    # create setters
+        # set fan speed
+        # set fan status
+        # set fan radius
+        # set fan color
+        ""
