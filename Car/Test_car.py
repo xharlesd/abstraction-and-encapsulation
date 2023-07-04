6a6d43c9a4f56c614c706e0b32113c402c537fc2
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-04 16:03:24 +0800

    accelerate 5 times

--- Car/Test_car.py
+++ Car/Test_car.py
@@ -5,4 +5,8 @@ from Car_class import Car
 My_car = Car()
 
 # call accelerate method five times and display current speed
+for i in range (5):
+    My_car.accelerate()
+    My_car.display_speed()
+
 # call brake method five times and display current speed
