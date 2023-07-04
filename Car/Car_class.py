76fa3505e840423b9d847019c16c9a8c545f73bf
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-04 15:54:53 +0800

    import car class and create car object

--- Car/Car_class.py
+++ Car/Car_class.py
@@ -1,11 +1,19 @@
 # Create Car class
 class Car:
     # constructor
-    def __init__ (self, model = 2023, make = "Ford Mustang GT", speed=0):
-        self.__year_model = model
+    def __init__ (self, year_model = 2023, make = "Ford Mustang GT", speed=0):
+        self.__year_model = year_model 
         self.__make = make
         self.__speed = speed
 
     # accelerate method add 5 each time it is called
+    def accelerate(self):
+        self.__speed += 5
+
     # brake method decrease 5 each time it is called 
+    def brake(self):
+        self.__speed -= 5
+
     # get current speed
+    def current_speed(self):
+        return self.__speed
--- Car/Test_car.py
+++ Car/Test_car.py
@@ -1,6 +1,8 @@
 # import car class
+from Car_class import Car
 
 # create car object 
+My_car = Car()
 
 # call accelerate method five times and display current speed
 # call brake method five times and display current speed
