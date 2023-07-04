33e4e833dcea01bdef34c45799e73058d5cea2b1
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-04 15:49:19 +0800

    create car class and create constructor

--- Car/Car_class.py
+++ Car/Car_class.py
@@ -1,5 +1,11 @@
 # Create Car class
+class Car:
     # constructor
+    def __init__ (self, model = 2023, make = "Ford Mustang GT", speed=0):
+        self.__year_model = model
+        self.__make = make
+        self.__speed = speed
+
     # accelerate method add 5 each time it is called
     # brake method decrease 5 each time it is called 
     # get current speed
