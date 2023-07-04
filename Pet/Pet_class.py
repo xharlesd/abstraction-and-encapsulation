91b8dc0ade4a3127b3d3d6eba8872b5fe0789301
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-04 18:28:45 +0800

    set and get name of pet

--- Pet/Pet_class.py
+++ Pet/Pet_class.py
@@ -9,10 +9,16 @@ class Pet:
 
     # setter methods
     # set name
+    def set_name(self, name):
+        self.__name = name
+
     # set animal type
     # set age
 
     # getter methods
     # get name
+    def set_name(self):
+        return self.__name
+    
     # get animal type
     # get age
