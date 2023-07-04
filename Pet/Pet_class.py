d04143b57aa42a56b3b095242b51d8df598bb226
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-04 18:06:57 +0800

    pet class constructor

--- Pet/Pet_class.py
+++ Pet/Pet_class.py
@@ -1,5 +1,11 @@
 # create pet class
+class Pet:
     # constructor
+    def __init__(self, name, animal_type, age):
+        # instance variables
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
 
     # setter methods
     # set name
